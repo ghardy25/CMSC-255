5f4b35989ee1fbeb3f345f56df90561e2822c2f4
ghardy25 <112019150+ghardy25@users.noreply.github.com>
2022-08-24 15:19:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**************************
+* Java Test Program
+***************************
+* Griffin Hardy
+* 8/24/2022
+* CMSC-255-001-FA2022
+**************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+	System.out.println("Griffin Hardy");
+	System.out.println("Java Test Program");
+	System.out.println("Test Java program to make sure everything is working");
+	System.out.println("Created 8/24/2022");
+	System.out.println("CMSC-255-001-FA2022");
+	System.out.println();
+	
+	}
+}
